d5fcb6b1aeacca28f9138a1a445a9cd6dc0201cd
AlexKat <katsubo79@gmail.com>
2024-05-23 02:17:44 +0300

    +

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components';
+import { AppRouter } from './components/AppRouter';
+import { Context } from '.';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useContext } from 'react';
+import { Loader } from './components/Loader';
+
+function App() {
+  const { auth } = useContext(Context);
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      {/* <BrowserRouter basename="/chat_fb"> */}
+      <Navbar />
+      <AppRouter />
+      {/* </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
